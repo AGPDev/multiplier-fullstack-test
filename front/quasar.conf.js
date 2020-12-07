8af33e04eba900fef3fdad5048740150d43d52a4
Anderson Guilherme Porto <anderson.porto@agpdev.com>
2020-12-07 19:27:34 -0300

    chore: add compatibilidade com Pug

--- front/quasar.conf.js
+++ front/quasar.conf.js
@@ -70,6 +70,11 @@ module.exports = function (/* ctx */) {
           loader: 'eslint-loader',
           exclude: /node_modules/
         })
+
+        cfg.module.rules.push({
+          test: /\.pug$/,
+          loader: 'pug-plain-loader'
+        })
       }
     },
 
