777444699fcdffe07f389ddd418dae10b6645e21
Anderson Guilherme Porto <anderson.porto@agpdev.com>
2020-12-08 15:14:15 -0300

    feat(components): fomulário padrão

---
+++ front/src/components/AppForm.vue
@@ -0,0 +1,34 @@
+<script>
+import { ValidationObserver } from 'vee-validate'
+
+export default {
+  name: 'AppForm',
+
+  components: {
+    ValidationObserver
+  },
+
+  props: {
+    submitAction: {
+      type: Function,
+      required: true
+    }
+  },
+
+  methods: {
+    reset () {
+      this.$refs.validationObserver.reset()
+    }
+  }
+}
+</script>
+
+<template lang="pug">
+  ValidationObserver(
+    slim
+    ref="validationObserver"
+    v-slot="{ handleSubmit }"
+  )
+    form(@submit.prevent="handleSubmit(submitAction)")
+      slot
+</template>
