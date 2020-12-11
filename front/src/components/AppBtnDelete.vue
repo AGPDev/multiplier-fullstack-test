862f6f4a81d34edea5bab4cd005d9285708b0f86
Anderson Guilherme Porto <anderson.porto@agpdev.com>
2020-12-11 17:45:15 -0300

    feat(front): botão generico para excluir items

---
+++ front/src/components/AppBtnDelete.vue
@@ -0,0 +1,48 @@
+<script>
+export default ({
+  name: 'AppBtnDelete',
+
+  props: {
+    deleteAction: {
+      type: Function,
+      default: () => {}
+    }
+  }
+})
+</script>
+
+<template lang="pug">
+  q-btn(
+    dense
+    flat
+    color="negative"
+    icon="mdi-delete"
+  )
+    q-menu(
+      auto-close
+      cover
+      anchor="center middle"
+    )
+      div(
+        class="row justify-center q-gutter-md q-pa-sm bg-warning"
+        style="width: 280px"
+      )
+        div(class="text-h6")
+          | Tem certeza disso?
+        div
+          q-btn(
+            unelevated
+            no-caps
+            class="q-mr-sm"
+            color="negative"
+            label="Sim, excluir"
+            @click="deleteAction"
+          )
+          q-btn(
+            unelevated
+            no-caps
+            class="q-ml-sm"
+            color="grey-8"
+            label="Não, cancelar"
+          )
+</template>
