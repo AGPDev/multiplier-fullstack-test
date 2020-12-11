8d2aac84df7d38f32d38a714a0d7edf5c3b04f15
Anderson Guilherme Porto <anderson.porto@agpdev.com>
2020-12-11 18:24:46 -0300

    feat(front): componente base para exibição de dialogs

---
+++ front/src/components/AppDialog.vue
@@ -0,0 +1,62 @@
+<script>
+export default {
+  name: 'AppDialog',
+
+  props: {
+    loading: {
+      type: Boolean
+    },
+    title: {
+      type: String,
+      default: ''
+    },
+    value: {
+      type: Boolean
+    }
+  },
+
+  data () {
+    return {
+      activeLocal: this.value
+    }
+  },
+
+  watch: {
+    value (val) {
+      this.activeLocal = val
+    }
+  }
+}
+</script>
+
+<template lang="pug">
+  q-dialog(
+    persistent
+    v-model="activeLocal"
+    @hide="$emit('input', false)"
+  )
+    q-card(style="min-width: 400px")
+      q-card-section(class="row items-center")
+        div(class="text-h6")
+          | {{ title }}
+        q-space
+        q-btn(
+          dense
+          flat
+          round
+          v-close-popup
+          icon="mdi-close"
+        )
+
+      div(class="row")
+        q-separator
+
+      q-inner-loading(:showing="loading")
+        q-spinner(
+          color="orange-14"
+          size="50px"
+        )
+
+      q-card-section
+        slot
+</template>
