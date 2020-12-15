4e2286a8a4e3157562fc36c76fe44d56083baec6
Anderson Guilherme Porto <anderson.porto@agpdev.com>
2020-12-15 20:14:25 -0300

    feat(front): filtro para truncar textos

--- front/src/boot/filters.js
+++ front/src/boot/filters.js
@@ -16,3 +16,11 @@ Vue.filter('formatPrice', function (val) {
 
   return parseFloat(val).toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })
 })
+
+Vue.filter('truncate', function (text, stop, clamp) {
+  if (text && text.length > stop) {
+    return text.slice(0, stop) + (stop < text.length ? clamp || '...' : '')
+  } else {
+    return text
+  }
+})
