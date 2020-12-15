b78069d34597688714be1ce281ee7d6ed602e3fe
Anderson Guilherme Porto <anderson.porto@agpdev.com>
2020-12-15 20:14:54 -0300

    feat(front): novas validações de quantidades

--- front/src/boot/vee-validate.js
+++ front/src/boot/vee-validate.js
@@ -6,14 +6,19 @@ import {
   setInteractionMode
 } from 'vee-validate'
 
+/* eslint camelcase: ["error", {ignoreImports: true}] */
 import {
   email,
-  required,
-  min
+  min,
+  min_value,
+  max_value,
+  required
 } from 'vee-validate/dist/rules'
 
 extend('email', email)
 extend('min', min)
+extend('min_value', min_value)
+extend('max_value', max_value)
 extend('required', required)
 
 localize('pt_BR', ptBR)
